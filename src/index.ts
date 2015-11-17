963469a473a6af91136a7e2a43be4a03772bc601
Kris Selden <kris.selden@gmail.com>
2015-11-17 15:05:43 -0800

    fix to invoke function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import nativePromise from './enqueue-microtask/native-promise';
 
-export const enqueueMicrotask : (microtask:()=>void)=>void = (function () {
+export const enqueueMicrotask = (function () : (microtask:()=>void)=>void {
 	if (nativePromise.supported) {
     	return nativePromise.enqueue;
     }
     throw new Error('no supported microtask queue in environment');
-});
+}());
